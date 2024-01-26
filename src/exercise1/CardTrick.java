6d49a6c66214104e10b32b8b786319ac91ad0ba3
hamzahnadeem16 <65934637+hamzahnadeem16@users.noreply.github.com>
2024-01-26 17:37:35 -0500

    commit: 2

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,57 +1,75 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
+ * It then searches the array of cards for the match to the user's card.
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Hamzah Nadeem
+ * @author
  */
 public class CardTrick {
-    
+
     public static void main(String[] args) {
-        
         Card[] hand = new Card[7];
 
+        // Fill the hand with random cards
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue(generateRandomNumber());
+            card.setSuit(Card.SUITS[generateRandomNumber(4)]);
+            hand[i] = card;
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        // Ask the user for Card value and suit
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick a card! Enter value (1-13): ");
+        int userValue = scanner.nextInt();
+        System.out.println("Enter suit (1-4): ");
+        int userSuit = scanner.nextInt();
+
+        // Create user's card
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(Card.SUITS[userSuit - 1]);
+
+        // Search for the user's card in the hand
+        boolean found = false;
+        for (Card card : hand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equals(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        // If the guess is successful, invoke the printInfo() method
+        if (found) {
+            printInfo();
+        } else {
+            System.out.println("Sorry, your card is not in the hand.");
+        }
     }
 
     /**
-     * A simple method to print out personal information. Follow the instructions to 
+     * A simple method to print out personal information. Follow the instructions to
      * replace this information with your own.
+     *
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
-    
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+
+        System.out.println("My name is Paul, but you can call me prof, Paul, or sir");
         System.out.println();
-        
+
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
+        System.out.println();
 
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
@@ -60,8 +78,26 @@ public class CardTrick {
         System.out.println("-- Riding my motorcycle");
 
         System.out.println();
-        
-    
     }
 
+    /**
+     * Generates a random number between 1 and 13 (inclusive).
+     *
+     * @return Random number
+     */
+    private static int generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(13) + 1;
+    }
+
+    /**
+     * Generates a random number between 0 and max (exclusive).
+     *
+     * @param max Maximum value (exclusive)
+     * @return Random number
+     */
+    private static int generateRandomNumber(int max) {
+        Random random = new Random();
+        return random.nextInt(max);
+    }
 }
